0f1e7d8656177f655455e70262be912dd9a855eb
slvgs <gabriellasilveriosantana15@gmail.com>
2022-10-11 15:39:31 -0300

    ExercicioCompleto

---
+++ src/CardNovo.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { InfoUsuario } from "./InfoUsuario"
+
+export function CardNovo (){
+
+    const titulo = "Título do vídeo";
+
+   const ReproduzVideo =  () => {
+    alert("O vídeo está sendo reproduzido");
+  }
+    return (
+        <div className="tela-inteira">
+        <header>
+          <h1>LabeTube</h1>
+          <input type="text" placeholder="Busca" id="campoDeBusca" />
+        </header>
+
+        <main>
+          <nav className="menu-vertical">
+            <ul>
+              <li className="botoes-meunu-vertical">Início</li>
+              <li className="botoes-meunu-vertical">Em alta</li>
+              <li className="botoes-meunu-vertical">Inscrições</li>
+              <hr />
+              <li className="botoes-meunu-vertical">Originais</li>
+              <li className="botoes-meunu-vertical">Histórico</li>
+            </ul>
+          </nav>
+
+          <section className="painel-de-videos">
+            <div className="box-pagina-principal" onClick={ReproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=1 " alt="" />
+              <h4>{titulo}</h4>
+              <InfoUsuario/>
+            </div>
+            <div className="box-pagina-principal" onClick={ReproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=2 " alt="" />
+              <h4>{titulo}</h4>
+              <InfoUsuario/>
+            </div>
+            <div className="box-pagina-principal" onClick={ReproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=3 " alt="" />
+              <h4>{titulo}</h4>
+              <InfoUsuario/>
+            </div>
+            <div className="box-pagina-principal" onClick={ReproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=4 " alt="" />
+              <h4>{titulo}</h4>
+              <InfoUsuario/>
+            </div>
+            <div className="box-pagina-principal" onClick={ReproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=5 " alt="" />
+              <h4>{titulo}</h4>
+              <InfoUsuario/>
+            </div>
+            <div className="box-pagina-principal" onClick={ReproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=6 " alt="" />
+              <h4>{titulo}</h4>
+              <InfoUsuario/>
+            </div>
+            <div className="box-pagina-principal" onClick={ReproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=7 " alt="" />
+              <h4>{titulo}</h4>
+              <InfoUsuario/>
+            </div>
+            <div className="box-pagina-principal" onClick={ReproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=8 " alt="" />
+              <h4>{titulo}</h4>
+              <InfoUsuario/>
+            </div>
+          </section>
+        </main>
+
+        <footer>
+          <h4>Oi! Eu moro no footer!</h4>
+        </footer>
+      </div>
+    )
+       
+    
+}
